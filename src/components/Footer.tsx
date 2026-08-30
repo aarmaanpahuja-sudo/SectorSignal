861b451f6fd60a7a3708c16a3560ca165900456a
aarmaanpahuja-sudo <aarmaan.pahuja@gmail.com>
2026-08-30 10:11:49 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="flex items-center justify-center gap-4">
 
           <p className="whitespace-nowrap">
-            © {new Date().getFullYear()} VexGrid - v1.5.0
+            © {new Date().getFullYear()} SectorSignal - v1.5.0
           </p>
 
           {/* Social Icons */}
